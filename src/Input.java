2ef72a8a9efcd96742f96a7f43d2c7d91d61b92f
code wizard <allanmaaz@Allans-MacBook-Pro.local>
2024-11-29 18:51:41 +0530

    two inputs

--- src/Input.java
+++ src/Input.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class Input {
     public static void main(String[]args){
+
         Scanner scanner =new Scanner(System.in);
-        System.out.println("ENTER your name:");
+        System.out.print("ENTER your name: ");
         String name=scanner.nextLine();
+        System.out.print("Enter your Age: ");
+        int age=scanner.nextInt();
         System.out.println("Hellow "+name);
+        System.out.println("Your Age is "+age);
     }
 }
