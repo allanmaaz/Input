c6552bd0603930aae5d691c0608412f224f76c74
code wizard <allanmaaz@Allans-MacBook-Pro.local>
2024-11-29 18:45:11 +0530

    INPUT

---
+++ src/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[]args){
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("ENTER your name:");
+        String name=scanner.nextLine();
+        System.out.println("Hellow "+name);
+    }
+}
